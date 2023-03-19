6e6b56ecf4c02ddf2d304db6572a17d461a7d013
M1kr03x <countdownlfg@mail.ru>
2023-03-20 02:12:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication21.cpp
@@ -0,0 +1,21 @@
+﻿#include <ctime>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+
+template <typename T1,typename T2>
+T1 sum(T1 a, T2 b) {
+	return a + b;
+}
+
+int main()
+{
+	int a;
+	std::cout << sum(5, 2.4);
+	
+	
+}
+
+
